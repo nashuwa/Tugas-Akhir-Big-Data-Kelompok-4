57f830b6c3351774c11aa4a972127a14a0334556
alipa97 <153807422+alipa97@users.noreply.github.com>
2025-06-18 19:57:32 +0700

    request lupa hehe

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 # from pymongo import MongoClient
 from db import collection_yfinance, collection_idx, collection_market_news
 from bson.json_util import dumps
